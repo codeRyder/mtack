09988f970877917a857b8a1a580aff15fdc8f5e9
Felix Hammer <felix@pixelschupser-nw.de>
2017-03-27 13:31:29 +0200

    Commit

---
+++ footer.php
@@ -0,0 +1,342 @@
+		</div>
+		<div class="mobile-nav-wrapper overlay">
+			<nav id="mobile-nav">
+				<ul>
+					<li><a href="#">Horse</a></li>
+					<li><a href="#">Fashion</a></li>
+					<li><a href="#">Dog</a></li>
+					<li><a href="#">Inlays</a></li>
+					<li><a href="#">Patches</a></li>
+					<li><a href="#">Trappings</a></li>
+					<li><a href="#">Marke</a></li>
+				</ul>
+			</nav>
+			<nav id="mobile-shop-nav">
+				<ul class="clearfix row">
+					<li class="col">
+						<a href="#" class="search">
+							<div class="primary">
+								<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<path fill="#864ea2" d="M23.413,20.576l-5.215-5.214c-0.027-0.027-0.06-0.047-0.088-0.073c1.024-1.556,1.624-3.418,1.624-5.422
+									C19.734,4.418,15.317,0,9.867,0C4.418,0,0,4.418,0,9.867c0,5.45,4.417,9.867,9.867,9.867c2.003,0,3.866-0.6,5.422-1.625
+									c0.025,0.029,0.045,0.062,0.073,0.089l5.214,5.215c0.783,0.782,2.054,0.782,2.837,0C24.195,22.63,24.195,21.359,23.413,20.576z
+									M9.867,16.313c-3.561,0-6.446-2.886-6.446-6.446S6.307,3.42,9.867,3.42s6.446,2.886,6.446,6.447S13.428,16.313,9.867,16.313z"/>
+								</svg>
+								Suche
+							</div>
+							<div class="close">
+								<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+								<rect x="-3" y="10.5" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 28.9706 11.9998)" fill="#fff" width="30" height="3"/>
+								<rect x="-3" y="10.5" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 12 28.9706)" fill="#fff" width="30" height="3"/>
+								</svg>
+								Close
+							</div>
+						</a>
+					</li>
+					<li class="col">
+						<a href="#">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 width="24px"  viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+								<path fill="#864ea2" d="M15.394,12.57c-0.193,0-0.334,0.108-0.428,0.325c-0.094,0.215-0.141,0.575-0.141,1.077
+									c0,0.52,0.047,0.885,0.141,1.096s0.234,0.319,0.428,0.319c0.203,0,0.351-0.108,0.442-0.319c0.093-0.211,0.139-0.576,0.139-1.096
+									c0-0.502-0.05-0.862-0.144-1.077C15.734,12.679,15.589,12.57,15.394,12.57z"/>
+								<path fill="#864ea2" d="M8.774,11.164c0.089-0.207,0.133-0.575,0.133-1.103c0-0.502-0.049-0.861-0.146-1.077
+									C8.665,8.769,8.519,8.66,8.326,8.66c-0.194,0-0.338,0.108-0.43,0.324C7.804,9.2,7.757,9.559,7.757,10.061
+									c0,0.52,0.047,0.885,0.139,1.097s0.236,0.317,0.43,0.317C8.537,11.475,8.687,11.372,8.774,11.164z"/>
+								<path fill="#864ea2" d="M22.707,13.41L24,12l-1.293-1.41l0.882-1.695l-1.611-1.027L22.394,6l-1.828-0.575l-0.08-1.911l-1.912-0.083
+									L18,1.608l-1.866,0.414l-1.027-1.613L13.41,1.292L12,0l-1.41,1.292L8.894,0.409L7.868,2.022L6,1.608L5.425,3.432L3.515,3.515
+									L3.431,5.425L1.608,6l0.413,1.868L0.409,8.895l0.884,1.695L0,12l1.293,1.41l-0.884,1.696l1.612,1.028L1.608,18l1.823,0.575
+									l0.083,1.91l1.911,0.08L6,22.394l1.868-0.416l1.026,1.613l1.696-0.883L12,24l1.41-1.292l1.696,0.883l1.027-1.613L18,22.394
+									l0.573-1.828l1.912-0.08l0.08-1.91L22.394,18l-0.416-1.865l1.611-1.028L22.707,13.41z M6.475,12.194
+									c-0.449-0.497-0.673-1.212-0.673-2.146c0-0.916,0.224-1.621,0.673-2.115c0.449-0.493,1.065-0.74,1.851-0.74
+									c0.784,0,1.4,0.25,1.85,0.747c0.448,0.498,0.673,1.206,0.673,2.121c0,0.934-0.225,1.647-0.673,2.141
+									c-0.45,0.494-1.066,0.74-1.85,0.74C7.541,12.941,6.924,12.693,6.475,12.194z M10.135,16.5H8.126l5.471-9h1.996L10.135,16.5z
+									 M17.244,16.112c-0.44,0.494-1.058,0.74-1.851,0.74c-0.782,0-1.396-0.246-1.836-0.746c-0.44-0.499-0.661-1.213-0.661-2.148
+									c0-0.915,0.22-1.62,0.661-2.114s1.054-0.74,1.836-0.74c0.793,0,1.41,0.247,1.851,0.74c0.438,0.494,0.661,1.204,0.661,2.128
+									C17.905,14.906,17.683,15.62,17.244,16.112z"/>
+							</svg>
+							Sale
+						</a>
+					</li>
+					<li class="col">
+						<a href="#">
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px"  viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+								<path fill="#864ea2" d="M12,11.226c1.505,0,2.793-0.548,3.855-1.645c1.067-1.096,1.6-2.418,1.6-3.969c0-1.548-0.532-2.871-1.6-3.967
+								C14.793,0.548,13.505,0,12,0c-1.506,0-2.792,0.548-3.857,1.645C7.078,2.741,6.545,4.064,6.545,5.612c0,1.55,0.533,2.873,1.598,3.969
+								C9.209,10.678,10.494,11.226,12,11.226z M21.949,18.702c-0.031-0.493-0.097-1.024-0.197-1.594c-0.1-0.569-0.226-1.098-0.377-1.587
+								c-0.151-0.485-0.354-0.961-0.611-1.425c-0.256-0.462-0.549-0.856-0.881-1.183c-0.331-0.327-0.736-0.587-1.215-0.782
+								c-0.477-0.195-1.006-0.292-1.582-0.292c-0.086,0-0.285,0.104-0.598,0.314c-0.312,0.209-0.666,0.442-1.058,0.701
+								c-0.395,0.26-0.903,0.494-1.533,0.702c-0.631,0.209-1.263,0.314-1.897,0.314s-1.267-0.105-1.896-0.314
+								c-0.629-0.208-1.143-0.442-1.534-0.702c-0.394-0.259-0.745-0.491-1.058-0.701C7.199,11.944,7,11.84,6.915,11.84
+								c-0.579,0-1.107,0.097-1.584,0.292c-0.479,0.194-0.883,0.455-1.215,0.782c-0.331,0.326-0.625,0.721-0.88,1.183
+								c-0.255,0.464-0.459,0.939-0.611,1.425c-0.151,0.489-0.277,1.018-0.376,1.587c-0.099,0.569-0.165,1.101-0.198,1.594
+								C2.017,19.194,2,19.697,2,20.214c0,1.169,0.346,2.094,1.037,2.771C3.728,23.661,4.647,24,5.792,24h12.416
+								c1.146,0,2.063-0.339,2.756-1.015C21.655,22.308,22,21.383,22,20.214C22,19.697,21.983,19.194,21.949,18.702z"/>
+							</svg>
+							Konto
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="search-wrapper overlay">
+			<div class="searchform">
+				<form action="" method="post">
+					<input type="text" name="suche" id="searchfield" placeholder="Suchtext eingeben" />
+					<input class="button ghost" type="submit" name="searchsubmit" value="Suche absenden" />
+				</form>
+			</div>
+		</div>
+		<div class="mini-cart-wrapper popout-box-wrapper">
+			<div class="mini-cart popout-box">
+				<h2>Im Warenkorb</h2>
+				<div class="cart-item-list">
+					<ul>
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+										<div class="cart-item-property">Farbe: braun</div>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+						<li>
+							<article class="row cart-item">
+								<a href="#" class="cart-item-delete col">
+									<img src="images/icons/delete.svg" alt="Löschen" />
+								</a>
+								<div class="cart-item-content-wrapper clearfix">
+									<div class="cart-item-content col">
+										<a href="#"><header><h3>Tuesday Collection: Mystic Gleam Patches</h3></header></a>
+										<div class="cart-item-property">Farbe: braun</div>
+									</div>
+									<div class="cart-item-price col">
+										39,99 €
+									</div>
+									<div class="cart-item-quantity col">
+										<input type="number" name="cart-item-1-quantity" value="1"/>
+									</div>
+									<div class="cart-item-total col">
+										1.239,99 €
+									</div>
+								</div>
+							</article>
+						</li>
+					</ul>
+				</div>
+				<div class="row row-padding cart-total-row">
+					<div class="col cart-total-label">
+						Warenkorbsumme (3 Artikel):
+					</div>
+					<div class="col cart-total">
+						2.165,89 €
+					</div>
+				</div>
+				<div class="row row-padding button-row">
+					<div class="col half">
+						<a href="/warenkorb" class=" button ghost">Zum Warenkorb</a>
+					</div>
+					<div class="col half">
+						<a href="/kasse" class=" button solid">Zur Kasse</a>
+					</div>
+					<div class="cart-info">
+						<strong>Hinweis: </strong>Gutscheincodes können auf der Warenkorbseite eingegeben werden.
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="popout-box-wrapper account-box-wrapper">
+			<div class="popout-box account-box">
+				<form action="" method="post">
+					<div class="row">
+						<div class="col half">
+							<div class="account-col">
+								<h2>Kundenlogin</h2>
+								<input class="login-input" type="email" name="email" id="email" />
+								<label for="email">E-Mail Adresse</label>
+								<input class="login-input" type="password" name="passwort" id="passwort" />
+								<div class="password-labels clearfix">
+									<label for="passwort">Passwort</label>
+									<a href="/recover-password">Passwort vergessen?</a>
+								</div>
+								<input class="button solid" type="submit" name="login-submit" value="Einloggen" />
+							</div>
+						</div>
+						<div class="col half">
+							<div class="account-col">
+							<h2>Noch kein Konto?</h2>
+							<p>
+								<strong>Profitiere von unserem Bonussystem</strong> und sammle bei jedem Deiner Einkäufe sog. <strong>Crystals</strong>.
+
+								Für Deine Crystals erhältst Du einen entsprechenden <strong>Rabatt bei Deiner nächsten Bestellung.</strong>
+							</p>
+							<a class="button ghost" href="/registrieren" >Benutzerkonto anlegen</a>
+						</div>
+					</div>
+
+					<div class="row button-row">
+						<div class="col half">
+						</div>
+						<div class="col half">
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="de">
+
+	<head>
+
+		<title>MagicTack | Ein Band - Viele Möglichkeiten</title>
+
+
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+		<meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui"/>
+		<meta name="mobile-web-app-capable" content="yes"/>
+		<meta name="application-name" content="MagicTack | Ein Band - Viele Möglichkeiten"/>
+		<meta name="format-detection" content="telephone=no"/>
+
+		<meta name="apple-mobile-web-app-capable" content="yes"/>
+		<meta name="apple-mobile-web-app-title" content="MagicTack | Ein Band - Viele Möglichkeiten"/>
+		<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="images/icons/icon-app.png"/>
+		<meta name="msapplication-TileImage" content="images/icons/icon_app.png">
+
+		<meta name="robots" content="index, follow"/>
+		<meta name="description" content=""/>
+
+		<meta property="og:title" content="MagicTack | Ein Band - Viele Möglichkeiten"/>
+		<meta property="og:site_name" content="MagicTack | Ein Band - Viele Möglichkeiten"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:url" content="http://www.magictack.de"/>
+		<meta property="og:description" content=""/>
+		<meta property="og:image" content="images/icons/icon-card.png"/>
+		<meta property="og:locale" content="de_de"/>
+
+		<link rel="icon" href="images/icons/icon-desktop.png"/>
+		<link rel="icon" href="images/icons/icon-app.png" sizes="192x192">
+		<link rel="shortcut icon" href="images/icons/icon-desktop.png"/>
+		<link rel="apple-touch-icon" href="images/icons/icon-app.png"/>
+		<link rel="apple-touch-icon-precomposed" href="/images/icons/icon-app.png"/>
+
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+
+		<!--[if lt IE 9]><script type="text/javascript" src="js/html5shiv.min.js"></script><![endif]-->
+
+	</head>
+
+	<body>
+		<div id="page">
+			<header id="header" class="clearfix">
+				<div class="logo">
+					<a href="." title="Zur Startseite">
+						<img src="images/magic-tack-logo.svg" alt="Magic Tack Logo Vector" />
+					</a>
+				</div>
+				<nav id="main-nav">
+					<ul>
+						<li><a href="#">Horse</a></li>
+						<li><a href="#">Fashion</a></li>
+						<li><a href="#">Dog</a></li>
+						<li><a href="#">Inlays</a></li>
+						<li><a href="#">Patches</a></li>
+						<li><a href="#">Trappings</a></li>
+						<li><a href="#">Marke</a></li>
+					</ul>
+				</nav>
+				<nav id="shop-nav">
+					<ul class="clearfix">
+						<li>
+							<a class="search" href="#">
+								<div class="primary">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<path fill="#818181" d="M23.413,20.576l-5.215-5.214c-0.027-0.027-0.06-0.047-0.088-0.073c1.024-1.556,1.624-3.418,1.624-5.422
+									C19.734,4.418,15.317,0,9.867,0C4.418,0,0,4.418,0,9.867c0,5.45,4.417,9.867,9.867,9.867c2.003,0,3.866-0.6,5.422-1.625
+									c0.025,0.029,0.045,0.062,0.073,0.089l5.214,5.215c0.783,0.782,2.054,0.782,2.837,0C24.195,22.63,24.195,21.359,23.413,20.576z
+									M9.867,16.313c-3.561,0-6.446-2.886-6.446-6.446S6.307,3.42,9.867,3.42s6.446,2.886,6.446,6.447S13.428,16.313,9.867,16.313z"/>
+									</svg>
+									Suche
+								</div>
+								<div class="close">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<rect x="-3" y="10.5" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 28.9706 11.9998)" fill="#fff" width="30" height="3"/>
+									<rect x="-3" y="10.5" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 12 28.9706)" fill="#fff" width="30" height="3"/>
+									</svg>
+									Schliessen
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<svg version="1.1" class="primary-icon"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									 width="24px"  viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<path fill="#818181" d="M15.394,12.57c-0.193,0-0.334,0.108-0.428,0.325c-0.094,0.215-0.141,0.575-0.141,1.077
+										c0,0.52,0.047,0.885,0.141,1.096s0.234,0.319,0.428,0.319c0.203,0,0.351-0.108,0.442-0.319c0.093-0.211,0.139-0.576,0.139-1.096
+										c0-0.502-0.05-0.862-0.144-1.077C15.734,12.679,15.589,12.57,15.394,12.57z"/>
+									<path fill="#818181" d="M8.774,11.164c0.089-0.207,0.133-0.575,0.133-1.103c0-0.502-0.049-0.861-0.146-1.077
+										C8.665,8.769,8.519,8.66,8.326,8.66c-0.194,0-0.338,0.108-0.43,0.324C7.804,9.2,7.757,9.559,7.757,10.061
+										c0,0.52,0.047,0.885,0.139,1.097s0.236,0.317,0.43,0.317C8.537,11.475,8.687,11.372,8.774,11.164z"/>
+									<path fill="#818181" d="M22.707,13.41L24,12l-1.293-1.41l0.882-1.695l-1.611-1.027L22.394,6l-1.828-0.575l-0.08-1.911l-1.912-0.083
+										L18,1.608l-1.866,0.414l-1.027-1.613L13.41,1.292L12,0l-1.41,1.292L8.894,0.409L7.868,2.022L6,1.608L5.425,3.432L3.515,3.515
+										L3.431,5.425L1.608,6l0.413,1.868L0.409,8.895l0.884,1.695L0,12l1.293,1.41l-0.884,1.696l1.612,1.028L1.608,18l1.823,0.575
+										l0.083,1.91l1.911,0.08L6,22.394l1.868-0.416l1.026,1.613l1.696-0.883L12,24l1.41-1.292l1.696,0.883l1.027-1.613L18,22.394
+										l0.573-1.828l1.912-0.08l0.08-1.91L22.394,18l-0.416-1.865l1.611-1.028L22.707,13.41z M6.475,12.194
+										c-0.449-0.497-0.673-1.212-0.673-2.146c0-0.916,0.224-1.621,0.673-2.115c0.449-0.493,1.065-0.74,1.851-0.74
+										c0.784,0,1.4,0.25,1.85,0.747c0.448,0.498,0.673,1.206,0.673,2.121c0,0.934-0.225,1.647-0.673,2.141
+										c-0.45,0.494-1.066,0.74-1.85,0.74C7.541,12.941,6.924,12.693,6.475,12.194z M10.135,16.5H8.126l5.471-9h1.996L10.135,16.5z
+										 M17.244,16.112c-0.44,0.494-1.058,0.74-1.851,0.74c-0.782,0-1.396-0.246-1.836-0.746c-0.44-0.499-0.661-1.213-0.661-2.148
+										c0-0.915,0.22-1.62,0.661-2.114s1.054-0.74,1.836-0.74c0.793,0,1.41,0.247,1.851,0.74c0.438,0.494,0.661,1.204,0.661,2.128
+										C17.905,14.906,17.683,15.62,17.244,16.112z"/>
+								</svg>
+								Sale
+							</a>
+						</li>
+						<li>
+							<a class="account-trigger" href="#">
+								<div class="primary">
+									<svg version="1.1" class="primary-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px"  viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+										<path fill="#818181" d="M12,11.226c1.505,0,2.793-0.548,3.855-1.645c1.067-1.096,1.6-2.418,1.6-3.969c0-1.548-0.532-2.871-1.6-3.967
+										C14.793,0.548,13.505,0,12,0c-1.506,0-2.792,0.548-3.857,1.645C7.078,2.741,6.545,4.064,6.545,5.612c0,1.55,0.533,2.873,1.598,3.969
+										C9.209,10.678,10.494,11.226,12,11.226z M21.949,18.702c-0.031-0.493-0.097-1.024-0.197-1.594c-0.1-0.569-0.226-1.098-0.377-1.587
+										c-0.151-0.485-0.354-0.961-0.611-1.425c-0.256-0.462-0.549-0.856-0.881-1.183c-0.331-0.327-0.736-0.587-1.215-0.782
+										c-0.477-0.195-1.006-0.292-1.582-0.292c-0.086,0-0.285,0.104-0.598,0.314c-0.312,0.209-0.666,0.442-1.058,0.701
+										c-0.395,0.26-0.903,0.494-1.533,0.702c-0.631,0.209-1.263,0.314-1.897,0.314s-1.267-0.105-1.896-0.314
+										c-0.629-0.208-1.143-0.442-1.534-0.702c-0.394-0.259-0.745-0.491-1.058-0.701C7.199,11.944,7,11.84,6.915,11.84
+										c-0.579,0-1.107,0.097-1.584,0.292c-0.479,0.194-0.883,0.455-1.215,0.782c-0.331,0.326-0.625,0.721-0.88,1.183
+										c-0.255,0.464-0.459,0.939-0.611,1.425c-0.151,0.489-0.277,1.018-0.376,1.587c-0.099,0.569-0.165,1.101-0.198,1.594
+										C2.017,19.194,2,19.697,2,20.214c0,1.169,0.346,2.094,1.037,2.771C3.728,23.661,4.647,24,5.792,24h12.416
+										c1.146,0,2.063-0.339,2.756-1.015C21.655,22.308,22,21.383,22,20.214C22,19.697,21.983,19.194,21.949,18.702z"/>
+									</svg>
+									Konto
+								</div>
+								<div class="close">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<rect x="-3" y="10.5" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 28.9706 11.9998)" fill="#fff" width="30" height="3"/>
+									<rect x="-3" y="10.5" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 12 28.9706)" fill="#fff" width="30" height="3"/>
+									</svg>
+									Schliessen
+								</div>
+							</a>
+						</li>
+						<li>
+							<a class="cart-trigger" href="warenkorb">
+								<div class="primary">
+									<svg version="1.1" class="primary-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									 width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<path fill="#818181" d="M7.2,19.203c-1.319,0-2.403,1.074-2.403,2.393C4.797,22.925,5.881,24,7.2,24c1.32,0,2.396-1.075,2.396-2.404
+									C9.596,20.277,8.521,19.203,7.2,19.203z M0,0v2.404h2.404l4.312,9.11L5.04,14.403c-0.118,0.359-0.243,0.833-0.243,1.191
+									c0,1.33,1.084,2.406,2.403,2.406h14.396v-2.406H7.678c-0.117,0-0.232-0.113-0.232-0.232v-0.126L8.521,13.2h8.879
+									c0.96,0,1.679-0.482,2.046-1.2l4.313-7.804C24,3.963,24,3.837,24,3.596c0-0.718-0.482-1.191-1.2-1.191H5.04L3.963,0H0z
+									 M19.203,19.203c-1.319,0-2.404,1.074-2.404,2.393c0,1.329,1.085,2.404,2.404,2.404s2.393-1.075,2.393-2.404
+									C21.596,20.277,20.522,19.203,19.203,19.203z"/>
+									</svg>
+								3 Artikel
+								</div>
+								<div class="close">
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									width="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+									<rect x="-3" y="10.5" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 28.9706 11.9998)" fill="#fff" width="30" height="3"/>
+									<rect x="-3" y="10.5" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 12 28.9706)" fill="#fff" width="30" height="3"/>
+									</svg>
+									Schliessen
+								</div>
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<a class="mobile-nav-trigger" href="#">
+					<div class="header-menu-bar"></div>
+					<div class="header-menu-bar"></div>
+					<div class="header-menu-bar"></div>
+					Menu
+				</a>
+			</header>
